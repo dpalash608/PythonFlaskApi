03a4a680ba6f950a30fb546f68cf75368d30a04d
I516845 <palash.das@sap.com>
2024-05-22 15:31:45 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	hello.py

--- text-processor-api.py
+++ text-processor-api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import os
 import google.generativeai as genai
+from flask_cors import CORS
 
 os.environ["GOOGLE_API_KEY"] = "AIzaSyBlZvQYgaofBGUaj4OBdFEXaRFyYHuhdJQ"
 
@@ -8,13 +9,15 @@ app = Flask(__name__)
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 
+CORS(app)  # This will enable CORS for all routes
+
 
 @app.route('/summary', methods=['GET'])
 def summary():
     data = request.args.get('data')
     response = model.generate_content(
         "summarize this html content in paragraph " + data)
-    return response
+    return response.text
 
 
 @app.route('/translator', methods=['GET'])
@@ -22,8 +25,8 @@ def translator():
     data = request.args.get('data')
     language = request.args.get('language')
     response = model.generate_content(
-        "translate this html content to " + language + " in paragraph " + data)
-    return response
+        "translate this html content" + data + " to " + language)
+    return response.text
 
 
 if __name__ == '__main__':
