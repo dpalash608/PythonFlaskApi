67917a10d405fa4c60dbf12123f1ccbc47640e0d
Palash Das <66236044+dpalash608@users.noreply.github.com>
2024-06-01 09:27:29 +0530

    Update text-processor-api.py

--- text-processor-api.py
+++ text-processor-api.py
@@ -3,7 +3,6 @@ import os
 import google.generativeai as genai
 from flask_cors import CORS
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyBlZvQYgaofBGUaj4OBdFEXaRFyYHuhdJQ"
 
 app = Flask(__name__)
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
